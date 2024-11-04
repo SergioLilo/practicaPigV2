5985dcc7208e60e07557c5cd93e6ba2e114bdff7
Sergio <sergioponceplaza@gmail.com>
2024-11-04 01:59:45 +0100

    2

--- app/src/main/java/com/example/practicapigv2/MainActivity2.kt
+++ app/src/main/java/com/example/practicapigv2/MainActivity2.kt
@@ -5,10 +5,17 @@ import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.a04_recyclerview.CustomAdapter
 import com.example.practicapigv2.databinding.ActivityMain2Binding
 
+val nombres = listOf(
+    "Aitor Tilla","Ana Conda", "Armando Broncas", "Aurora Boreal",
+    "Bartolo Mesa","Carmen Mente", "Dolores Delirio", "Elsa Pato",
+    "Enrique Cido","Esteban Dido", "Elba Lazo", "Fermin Tado",
+    "Lola Mento", "Luz Cuesta", "Margarita Flores", "Paco Tilla",
+    "Pere Gil", "Pío Nono", "Salvador Tumbado", "Zoila Vaca"
+)
+ lateinit var   nombresEleccion: ArrayList<String>
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,46 +24,36 @@ class MainActivity2 : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var numeroElecciones:Int=0
         val numJugadores = intent.getStringExtra("numJugadores")?.toInt()?:0
-        val nombres = listOf(
-            "Aitor Tilla","Ana Conda", "Armando Broncas", "Aurora Boreal",
-            "Bartolo Mesa","Carmen Mente", "Dolores Delirio", "Elsa Pato",
-            "Enrique Cido","Esteban Dido", "Elba Lazo", "Fermin Tado",
-            "Lola Mento", "Luz Cuesta", "Margarita Flores", "Paco Tilla",
-            "Pere Gil", "Pío Nono", "Salvador Tumbado", "Zoila Vaca"
+        //ponerRecicleViews(numJugadores, nombres)
+        val recyclerViews = listOf(
+            binding.rec1,
+            binding.rec2,
+            binding.rec3,
+            binding.rec4
         )
 
-        var listaRecicle= ArrayList<RecyclerView>()
+        for (i in 0 until numJugadores) {
+            recyclerViews[i].apply {
+                visibility = View.VISIBLE
+                layoutManager = LinearLayoutManager(this@MainActivity2)
+                adapter = CustomAdapter(nombres)
 
-        listaRecicle.add(binding.rec1)
-        listaRecicle.add(binding.rec2)
-        listaRecicle.add(binding.rec3)
-        listaRecicle.add(binding.rec4)
 
-        /*
-        for (recyclerView in listaRecicle) {
-            recyclerView.visibility = View.GONE
+            }
         }
 
-        listaRecicle.get(0).visibility=View.VISIBLE
-        val recyclerView: RecyclerView = listaRecicle[0]
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val customAdapter = CustomAdapter(nombres)
-        recyclerView.adapter = customAdapter*/
 
-        /*
-        for (i in 0 until numJugadores)  {
-            if (i < listaRecicle.size) { // Asegurarse de no exceder el tamaño de la lista
-                listaRecicle[i].visibility = View.VISIBLE
-                val recyclerView: RecyclerView = listaRecicle[i]
-                recyclerView.layoutManager = LinearLayoutManager(this)
 
-                // Puedes pasar datos específicos a cada adaptador si es necesario
-                // Aquí se asigna el mismo conjunto de nombres para simplificar
-                val customAdapter = CustomAdapter(nombres) // Puedes modificar esto si deseas diferentes datos por RecyclerView
-                recyclerView.adapter = customAdapter
 
-                }
-            }*/
-        }
+
+
+
+
     }
+
+    private fun ponerRecicleViews(numJugadores: Int, nombres: List<String>) {
+
+    }
+}
