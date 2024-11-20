edd23115be1af10ecdd1ca8b978cc264bb472107
SergioLilo <sergioponceplaza@gmail.com>
2024-11-20 11:05:53 +0100

    register hecho v1.0

--- app/src/main/java/com/example/practicapigv2/Registro_HUB/BBDD/UsuarioDao.kt
+++ app/src/main/java/com/example/practicapigv2/Registro_HUB/BBDD/UsuarioDao.kt
@@ -2,10 +2,17 @@ package com.example.practicapigv2.Registro_HUB.BBDD
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface UsuarioDao {
 
     @Insert
      fun insertarUsuario(usuario: Usuario)
+
+     @Query("Select * from usuario where usuario_nombre= :nombreIntroducido")
+     fun comprobarPorNombre(nombreIntroducido:String):Usuario?
+    @Query("Select * from usuario")
+    fun obtenerTodosUsuarios(): List<Usuario>
+
 }
--- app/src/main/java/com/example/practicapigv2/Registro_HUB/RegistroLogin.kt
+++ app/src/main/java/com/example/practicapigv2/Registro_HUB/RegistroLogin.kt
@@ -2,8 +2,10 @@ package com.example.practicapigv2.Registro_HUB
 
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,16 +13,21 @@ import androidx.lifecycle.lifecycleScope
 import com.example.practicapigv2.R
 import com.example.practicapigv2.Registro_HUB.BBDD.DataBaseUsuario
 import com.example.practicapigv2.Registro_HUB.BBDD.Usuario
+import com.example.practicapigv2.Registro_HUB.BBDD.UsuarioDao
 import com.example.practicapigv2.databinding.ActivityHubBinding
 import com.example.practicapigv2.databinding.ActivityRegistroLoginBinding
 import com.example.practicapigv2.juegoDado.MainActivity2
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
 
 class RegistroLogin : AppCompatActivity() {
     lateinit var binding: ActivityRegistroLoginBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -38,29 +45,95 @@ class RegistroLogin : AppCompatActivity() {
             startActivity(intent)
         }
 
-        lifecycleScope.launch {
-            withContext(Dispatchers.IO) {
+
+
+
         val userDao = DataBaseUsuario.getDatabase(this@RegistroLogin).usuarioDao()
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO){
+                userDao.obtenerTodosUsuarios()
+            }
 
+        }
         binding.registerID.setOnClickListener {
+                    var nombreLength:Boolean=false
+                    var contrasenyaLength:Boolean=false
+                    var edadCorrecta:Boolean=false
 
-            lifecycleScope.launch {
-                withContext(Dispatchers.IO) {
                     val usuario: Usuario = Usuario(
                         nombre = binding.nombreID.text.toString(),
                         contrasenya = binding.contrasenyaID.text.toString(),
                         fechaNacimiento = binding.datePickerID.text.toString()
                     )
-                    userDao.insertarUsuario(usuario)
+                    comprobacionUser(usuario, nombreLength, contrasenyaLength, edadCorrecta,userDao)
+        }
+    }
+    private fun resetMensajes(){
+        binding.nombreIncorrectoID.text=""
+        binding.contrasenyaIncorrectoID.text=""
+        binding.fechaIncorrectaID.text=""
+        binding.userExistenteID.text=""
+    }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private  fun comprobacionUser(
+        usuario: Usuario,
+        nombreLength: Boolean,
+        contrasenyaLength: Boolean,
+        edadCorrecta: Boolean,
+        userDao:UsuarioDao
+    ) {
+        resetMensajes()
+        lifecycleScope.launch {
+            var nombreLength1 = nombreLength
+            var contrasenyaLength1 = contrasenyaLength
+            var edadCorrecta1 = edadCorrecta
+
+            var usuarioExistente:Boolean=false
+            withContext(Dispatchers.IO) {
+
+                if (usuario.nombre.length in 4..10) {
+                    nombreLength1 = true
+                }
+                if ((usuario.contrasenya.length in 4..10)) {
+
+                    for (i in 0..9) {
+                        if (usuario.contrasenya.contains(i.toString())) {
+                            contrasenyaLength1 = true
+                        }
+                    }
+                }
+                edadCorrecta1 = esMenorDe16Anios(usuario.fechaNacimiento)
+                if (nombreLength1 && contrasenyaLength1 && edadCorrecta1) {
+                    val comprobacionNombre = userDao.comprobarPorNombre(usuario.nombre)
+                    if (comprobacionNombre == null) {
+                        userDao.insertarUsuario(usuario)
+                    }else{
+                        usuarioExistente=true
+                    }
+                }
+
+            }
+            withContext(Dispatchers.Main) {
+                if (usuarioExistente) {
+                    binding.userExistenteID.text = "Usuario ya existente, cambie el nombre"
+                }
+
+             if (!nombreLength1) {
+
+                binding.nombreIncorrectoID.text = "El nombre debe tener entre 4 y 10 caracteres"
+        }
+                if (!contrasenyaLength1) {
+
+                binding.contrasenyaIncorrectoID.text =
+                    "Contraseña entre 4 y 10 caracteres y debe contener un numero"
+        }
+                if (!edadCorrecta1) {
+
+                binding.fechaIncorrectaID.text = "Debes tener mas de 16 años"
                 }
             }
-
         }
-
-    }
-}
-
 
     }
 
@@ -79,5 +152,27 @@ class RegistroLogin : AppCompatActivity() {
             }, year, month, day
         )
         datePickerDialog.show()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+
+    fun esMenorDe16Anios(fechaNacimientoString: String): Boolean {
+
+        var formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val comprobacion = fechaNacimientoString.split("-")
+        val dia=comprobacion[0]
+        val mes=comprobacion[1]
+
+
+        if (dia.length==1) formatter = DateTimeFormatter.ofPattern("d-MM-yyyy")
+        if (mes.length==1)formatter = DateTimeFormatter.ofPattern("dd-M-yyyy")
+        if (mes.length==1&&dia.length==1)formatter = DateTimeFormatter.ofPattern("d-M-yyyy")
+
+
+        val fechaNacimiento = LocalDate.parse(fechaNacimientoString, formatter)
+        val fechaActual = LocalDate.now()
+
+        val edad = Period.between(fechaNacimiento, fechaActual).years
+
+        return edad>16
     }
 }
